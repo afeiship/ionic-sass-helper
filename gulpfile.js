0c1c15849e402a034e05fe987374d3da408bc71b
feizheng <1290657123@qq.com>
2016-10-11 10:13:25 +0800

    update base modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,23 @@
-/**
- *  Welcome to your gulpfile!
- *  The gulp tasks are splitted in several files in the gulp directory
- *  because putting all here was really too long
- */
+(function() {
 
-'use strict';
+  'use strict';
 
-var gulp = require('gulp');
-var path = require('path');
-var sass = require('gulp-sass');
-var del = require('del');
-var autoprefixer = require('gulp-autoprefixer');
-var debug = require('gulp-debug');
-var sourcemaps = require('gulp-sourcemaps');
-var uglify = require('gulp-uglify');
-var concat = require('gulp-concat');
-var rename = require('gulp-rename');
-var cssmin = require('gulp-minify-css');
-var config = {
-  sassOptions: {
-    outputStyle: 'expanded' /* nested | expanded | compact | compressed */
-  },
-  src: './src',
-  dist: './dist'
-};
+  var path = require('path');
+  var gulp = require('gulp');
+  var argv = require('yargs').argv;
+  var fs = require('fs');
+  var $ = require('gulp-load-plugins')({
+    pattern: ['gulp-*', 'gulp.*', 'del']
+  });
+
+  //import
+  fs.readdirSync('./gulp').map(function(file) {
+    require('./gulp/' + file);
+  });
 
 
+  gulp.task('default', ['clean'], function() {
+    gulp.start(['styles']);
+  });
 
-gulp.task('clean', function () {
-  return del(config.dist);
-});
-
-gulp.task('sass', function () {
-  return gulp.src(config.src + '/style.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass(config.sassOptions).on('error', sass.logError))
-    .pipe(autoprefixer('last 2 version'))
-    .pipe(gulp.dest(config.dist))
-    .pipe(cssmin())
-    .pipe(rename({
-      suffix: ".min",
-      extname: ".css"
-    }))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(config.dist));
-});
-
-
-gulp.task('default', ['clean'], function () {
-  gulp.start(['sass']);
-});
+}());
